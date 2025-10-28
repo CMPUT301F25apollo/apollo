ef5d43a35c27cf9b2c3dc9baad97e1f6d0fe8422
marvinuofa <mcu@ualberta.ca>
2025-10-28 08:46:59 -0600

    fixed event inflater

--- apollo/app/src/main/java/com/example/apollo/ui/organizer/events/EventsFragment.java
+++ apollo/app/src/main/java/com/example/apollo/ui/organizer/events/EventsFragment.java
@@ -4,20 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.apollo.R;
 import com.example.apollo.databinding.FragmentEventsBinding;
-
 
 public class EventsFragment extends Fragment {
 
     private FragmentEventsBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         EventsViewModel eventsViewModel = new ViewModelProvider(this).get(EventsViewModel.class);
@@ -25,8 +29,18 @@ public class EventsFragment extends Fragment {
         binding = FragmentEventsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textEvents;
+        // Access views through binding
+        Button waitlistButton = binding.buttonEventWaitlist;
+        TextView textView = binding.textEvents;
+
+        waitlistButton.setOnClickListener(v -> {
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.navigation_event_waitlist);
+        });
+
+        // Example of using ViewModel text
         eventsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
--- apollo/app/src/main/java/com/example/apollo/ui/organizer/profile/ProfileFragment.java
+++ apollo/app/src/main/java/com/example/apollo/ui/organizer/profile/ProfileFragment.java
@@ -4,29 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.apollo.databinding.FragmentProfileBinding;
+import com.example.apollo.databinding.FragmentProfileOrganizerBinding;
 
 public class ProfileFragment extends Fragment {
 
-    private FragmentProfileBinding binding;
+    private FragmentProfileOrganizerBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textProfile;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileOrganizerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -34,4 +23,5 @@ public class ProfileFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
