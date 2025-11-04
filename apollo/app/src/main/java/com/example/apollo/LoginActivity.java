94b12fb94ed3f3cc660a3a1b051b003a743ef266
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2025-11-03 23:00:24 -0700

    Add login and signup activities, update profile and settings fragments

---
+++ apollo/app/src/main/java/com/example/apollo/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.example.apollo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewSignUp;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Auto-login
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+            return; // Stop executing rest of the onCreate
+        }
+
+        // Continue to show login screen
+        setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth & UI components
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewSignUp = findViewById(R.id.textViewSignUp);
+
+        // Login button click
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                // Validation checks
+                if (TextUtils.isEmpty(email)) {
+                    editTextEmail.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    editTextPassword.setError("Password is required.");
+                    return;
+                }
+
+                //  Firebase login
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Login successful.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    // Go to MainActivity
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish(); // Close LoginActivity
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed: "
+                                                    + task.getException().getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        //  Sign up text click
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+}
